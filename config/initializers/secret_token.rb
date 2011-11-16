2d29cce9b8a08ff85cd3e3beaf200da3af036e9f
Feil Ferenc <fulke74@gmail.com>
2011-11-16 19:48:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanergyKft::Application.config.secret_token = '3b3c88e65570a32f2321d70c4a43c503f026e830c099ce20a5fd258770953c45355de2e946aa49456c8b15b4e44f0f3407860275f646f6cb772950cda2ccc96c'
